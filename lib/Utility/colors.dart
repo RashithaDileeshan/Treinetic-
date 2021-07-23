b5b47954fe2d93a89523daba8bf14a260a6e56dc
RashithaDileeshan <rashbatugedara9@gmail.com>
2021-07-23 06:47:19 +0530

    Change the color page

--- lib/Utility/colors.dart
+++ lib/Utility/colors.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  static const niceBlue = const Color(0xff0c4eb0);
-  static const pinkishGrey = const Color(0xffbababa);
-  static const paleLilac = const Color(0xffe6e4ff);
-  static const paleLilaTtwo = const Color(0xffd7d4ff);
-  static const whiteTwo = const Color(0xffffffff);
-  static const white = const Color(0xffd9d9d9);
-  static const metallicBlue = const Color(0xff526e88);
-  static const perrywinkle = const Color(0xff0d47a1);
-  static const lightTeal = const Color(0xff84ddad);
-  static const dodgerBlue = const Color(0xff4892ff);
-  static const lightPeriwinkle = const Color(0xffd1cdfd);
-  static const torchRed = const Color(0xffff002e);
   static const blackPearl2 = const Color(0xff091b2d);
-  static const green = const Color(0xff17d26d);
   static const indigo = const Color(0xff9fa8da);
   static const gray = const Color(0xff37474f);
   static const gray2 = const Color(0xff607d8b);
   static const indigoShade = const Color(0xffe8eaf6);
   static const grey50 = const Color(0xfffafafa);
-  static const disable = const Color(0xffdde4f1);
-  static const red = const Color(0xFFB71C1C);
 }
